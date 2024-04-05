49274635c48ff60ec09689d8616551267a1fee0b
Andy Grigg <andrew.grigg@ansys.com>
2024-04-05 13:08:35 -0400

    Fix docstring

--- src/ansys/grantami/jobqueue/_connection.py
+++ src/ansys/grantami/jobqueue/_connection.py
@@ -140,8 +140,7 @@ class JobQueueApiClient(ApiClient):
     @property
     def is_admin_user(self) -> bool:
         """
-        Flag indicating if the current user is an
-        administrator of the job queue.
+        Flag indicating if the current user is an administrator of the job queue.
 
         Administrators can promote jobs to the top of the queue and interact with other users' jobs.
 
